62e26410518a978f81aa6cedf13185ef43c4379a
sakkuru <sakkuru0323@gmail.com>
2017-12-26 12:33:51 +0900

    update README.md

--- README.md
+++ README.md
@@ -1,10 +1,18 @@
-# How to use;
+# How to use
+
 ```
-git clone https://github.com/sakkuru/SAS-Token-Generator.git
-cd SAS-Token-Generator
-node app.js "Endpoint=sb://XXX.servicebus.windows.net/;SharedAccessKeyName=..."
+> git clone https://github.com/sakkuru/SAS-Token-Generator.git
+> cd SAS-Token-Generator
+> node app.js '<Connection String>' <- Quoted.
 -------------------------------------------------
-Endpoint: https://XXX.servicebus.windows.net/YYY/publishers/test/messages
-SAS Token: SharedAccessSignature sr=...&sig=...&se=...&skn=...re
+HTTPS Endpoint: <HTTPS Endpoint>
+SAS Token: <SAS Token>
 -------------------------------------------------
+
+> curl -i -H '<SAS Token>' -d {"hoge":"fuga"} <HTTPS Endpoint>
+HTTP/2 201
+content-type: application/xml; charset=utf-8
+server: Microsoft-HTTPAPI/2.0
+strict-transport-security: max-age=31536000
+date: Tue, 26 Dec 2017 03:13:30 GMT
 ```
--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const utf8 = require('utf8');
 const crypto = require('crypto');
 
-// const createSharedAccessToken = (uri, saName, saKey) => {
 const createSharedAccessToken = connectionString => {
     const endpoint = connectionString.split('Endpoint=')[1].split(";")[0];
     const saName = connectionString.split('SharedAccessKeyName=')[1].split(";")[0];
@@ -12,23 +11,25 @@ const createSharedAccessToken = connectionString => {
         throw "Missing required parameter";
     }
 
-    const uri = 'https:' + endpoint.split(":")[1] + entity + '/publishers/test/messages';
-    const encoded = encodeURIComponent(uri);
+    const httpsEndpoint = 'https:' + endpoint.split(":")[1] + entity + '/publishers/test/messages';
+    const encoded = encodeURIComponent(httpsEndpoint);
+
     const now = new Date();
     const week = 60 * 60 * 24 * 7;
     const ttl = Math.round(now.getTime() / 1000) + week;
     const signature = encoded + '\n' + ttl;
     const signatureUTF8 = utf8.encode(signature);
     const hash = crypto.createHmac('sha256', saKey).update(signatureUTF8).digest('base64');
+
     return [
         'SharedAccessSignature sr=' + encoded + '&sig=' +
         encodeURIComponent(hash) + '&se=' + ttl + '&skn=' + saName,
-        uri
+        httpsEndpoint
     ];
 }
 
 const [sasToken, endpoint] = createSharedAccessToken(process.argv[2]);
 console.log('-------------------------------------------------');
-console.log('Endpoint:', endpoint);
+console.log('HTTPS Endpoint:', endpoint);
 console.log('SAS Token:', sasToken);
 console.log('-------------------------------------------------');
